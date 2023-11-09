87abcbf7e2a47ecdc4691d1b4b34ea8570514d79
Yaroslava <yaroslava.hovenko@gmail.com>
2023-11-09 22:54:14 +0200

    final change

--- js/task-2.js
+++ js/task-2.js
@@ -36,6 +36,7 @@ const allUsers = [
 
 const getUsersWithFriend = (users, friendName) =>
   users.filter(({ friends }) => friends.includes(friendName));
+
 console.log(getUsersWithFriend(allUsers, 'Briana Decker'));
 // [
 //   {
